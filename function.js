066abe0252f1702c57c600be7244f9fc109ef9df
haolong1016 <164804986+haolong1016@users.noreply.github.com>
2024-04-28 20:37:59 -0500

    Delete function.js

--- function.js
+++
@@ -1,9 +0,0 @@
-function lastItem(fruits, outputId) {
-    fruits.sort((a, b) => a.localeCompare(b));
-    const lastAlphabetical = fruits[fruits.length - 1];
-    const outputDiv = document.getElementById(outputId);
-    if (!outputDiv.innerHTML) {
-        outputDiv.innerHTML = `Fruits sorted: ${fruits.join(',')}<br>Last Alphabetical Item: ${lastAlphabetical}`;
-    }
-    toggleVisibility(outputId);
-}
